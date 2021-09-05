8e8f28a18d143bca254d9980611ea6b8ef878907
Martin Corredor <dev.martincorredor@gmail.com>
2021-09-04 19:44:08 -0500

    |FIXED| Fix sintax errors

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
-"""Python module"""
+""" log parsing """
+
+
 import sys
 
 
-def stats(size, counter_for_codes):
-    """print the computes metric"""
+def printx(data, status):
+    """ print the log """
+    print("File size: {}".format(data))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
-    print("File size: {:d}".format(size))
-    for k, v in sorted(counter_for_codes.items()):
-        print("{}: {}".format(k, v))
-
-
-counter_for_codes = {}
+status = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0}
 counter = 0
-size = 0
-status = ["200", "301", "400", "401", "403", "404", "405", "500"]
-
-string = "GET /projects/260 HTTP/1.1"
-
+data = 0
 try:
     for line in sys.stdin:
-        if string in line:
-            split = line.split(' ')
-            if len(split) < 4:
-                continue
-            try:
-                size += int(split[-1])
-            except Exception:
-                continue
-            comand = split[-2]
-            if comand in status:
-                if comand in counter_for_codes.keys():
-                    counter_for_codes[comand] += 1
-                else:
-                    counter_for_codes[comand] = 1
-        counter += 1
-        if counter % 10 == 0:
-            stats(size, counter_for_codes)
-    else:
-        stats(size, counter_for_codes)
-except KeyboardInterrupt:
-    stats(size, counter_for_codes)
+        if counter == 10:
+            printx(data, status)
+            counter = 1
+        else:
+            counter = counter + 1
+        parsed = line.split()
+        try:
+            data = data + int(parsed[-1])
+        except Exception as e:
+            pass
+        try:
+            for key, value in status.items():
+                if key == parsed[-2]:
+                    status[key] = status[key] + 1
+        except Exception as e:
+            pass
+    printx(data, status)
+except KeyboardInterrupt as e:
+    printx(data, status)
