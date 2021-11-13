fe90782f1d0fc859e34581fa0fa924adad6286b1
Martin Corredor <dev.martincorredor@gmail.com>
2021-11-13 15:39:09 -0500

    |ADDED| Star Wars Characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const query = require('request');
+const filmID = process.argv[2];
+
+const names = [];
+let char = [];
+
+query(
+  `https://swapi-api.hbtn.io/api/films/${filmID}`,
+  function (error, response, body) {
+    if (error !== null) {
+      console.error('error:', error);
+    }
+    const data = JSON.parse(body);
+    char = data.characters;
+    char.forEach((character, idx) => {
+      query(`${character}`, function (error, response, body) {
+        if (error !== null) {
+          console.error('error:', error);
+        }
+        const data = JSON.parse(body);
+        names[idx] = data.name;
+        if (names.length === char.length && !names.includes(undefined)) {
+          names.forEach((name) => {
+            console.log(name);
+          });
+        }
+      });
+    });
+  }
+);
